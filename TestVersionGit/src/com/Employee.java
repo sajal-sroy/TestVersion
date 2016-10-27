87ac496992d393214f0b9f27a75843724e2b52d3
sajal.sinha.roy <sajal.sinha.roy@P3ALAP-5464ZBC.dir.svc.accenture.com>
2016-10-27 12:05:58 +0530

    Initial Commit

---
+++ TestVersionGit/src/com/Employee.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Employee {
+
+	Employee(){
+		System.out.println("hello");
+	}
+}
---
+++ TestVersionGit/src/com/Main.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Main {
+ public static void main(String[] args) {
+	Employee emp=new Employee();
+	System.out.println(emp.hashCode()); 
+}
+}
